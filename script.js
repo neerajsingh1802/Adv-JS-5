359ceb1c8463457459e8e99cf9fcc3984f43eb61
MR. NEERAJ SINGH <139963133+neerajsingh1802@users.noreply.github.com>
2023-11-21 23:56:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// 1.
+// let Book={
+//     name:"advance js",
+//     page:200,
+//     price:150
+// };
+// let{name,page,price}=Book;
+// console.log(price);
+// console.log(name);
+// console.log(page);
+
+
+// 2.
+
+// let Book={
+//         fName:"Neeraj",
+//         name:"advance js",
+//         page:200,
+//         price:150
+//      };
+//      let{fName,name:title,page,price}=Book;
+//      console.log(title);
+
